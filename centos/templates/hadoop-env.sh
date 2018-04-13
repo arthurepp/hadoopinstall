59565e15a6393915a6d492cbcd6ed1fe272fa3ce
Arthur Epprecht <arthur.epp@gmail.com>
2018-04-13 13:23:54 -0300

    Update hadoop-env.sh

--- centos/templates/hadoop-env.sh
+++ centos/templates/hadoop-env.sh
@@ -1,3 +1,3 @@
 export JAVA_HOME="/usr/lib/jvm/java-1.8.0-openjdk/"
 export HADOOP_OPTS="$HADOOP_OPTS -Djava.net.preferIPv4Stack=true"
-export HADOOP_CONF_DIR="/hadoop-2.8.2/etc/hadoop"
+export HADOOP_CONF_DIR="/usr/local/hadoop/etc/hadoop"
