ce82039c447b4bb76740fca6fa86c46fe1180dc2
Arthur Epprecht <arthur.epp@gmail.com>
2018-04-15 14:48:18 -0300

    copy required files

--- centos/master2.sh
+++ centos/master2.sh
@@ -9,9 +9,41 @@ ssh-copy-id -i ~/.ssh/id_rsa.pub hadoop@hadoop-slave3
 
 sudo chmod 0600 ~/.ssh/authorized_keys
 
-scp -r /usr/local/hadoop/* hadoop@hadoop-slave1:/usr/local/hadoop
-scp -r /usr/local/hadoop/* hadoop@hadoop-slave2:/usr/local/hadoop
-scp -r /usr/local/hadoop/* hadoop@hadoop-slave3:/usr/local/hadoop
+scp -r /usr/local/hadoop/bin hadoop@hadoop-slave1:/usr/local/hadoop
+scp -r /usr/local/hadoop/hdfs hadoop@hadoop-slave1:/usr/local/hadoop
+scp -r /usr/local/hadoop/include hadoop@hadoop-slave1:/usr/local/hadoop
+scp -r /usr/local/hadoop/libexec hadoop@hadoop-slave1:/usr/local/hadoop
+scp -r /usr/local/hadoop/logs hadoop@hadoop-slave1:/usr/local/hadoop
+scp -r /usr/local/hadoop/share hadoop@hadoop-slave1:/usr/local/hadoop
+scp -r /usr/local/hadoop/etc hadoop@hadoop-slave1:/usr/local/hadoop
+scp -r /usr/local/hadoop/lib hadoop@hadoop-slave1:/usr/local/hadoop
+scp -r /usr/local/hadoop/slib hadoop@hadoop-slave1:/usr/local/hadoop
+scp -r /usr/local/hadoop/spark hadoop@hadoop-slave1:/usr/local/hadoop
+scp -r /usr/local/hadoop/tmp hadoop@hadoop-slave1:/usr/local/hadoop
+
+scp -r /usr/local/hadoop/bin hadoop@hadoop-slave2:/usr/local/hadoop
+scp -r /usr/local/hadoop/hdfs hadoop@hadoop-slave2:/usr/local/hadoop
+scp -r /usr/local/hadoop/include hadoop@hadoop-slave2:/usr/local/hadoop
+scp -r /usr/local/hadoop/libexec hadoop@hadoop-slave2:/usr/local/hadoop
+scp -r /usr/local/hadoop/logs hadoop@hadoop-slave2:/usr/local/hadoop
+scp -r /usr/local/hadoop/share hadoop@hadoop-slave2:/usr/local/hadoop
+scp -r /usr/local/hadoop/etc hadoop@hadoop-slave2:/usr/local/hadoop
+scp -r /usr/local/hadoop/lib hadoop@hadoop-slave2:/usr/local/hadoop
+scp -r /usr/local/hadoop/slib hadoop@hadoop-slave2:/usr/local/hadoop
+scp -r /usr/local/hadoop/spark hadoop@hadoop-slave2:/usr/local/hadoop
+scp -r /usr/local/hadoop/tmp hadoop@hadoop-slave2:/usr/local/hadoop
+
+scp -r /usr/local/hadoop/bin hadoop@hadoop-slave3:/usr/local/hadoop
+scp -r /usr/local/hadoop/hdfs hadoop@hadoop-slave3:/usr/local/hadoop
+scp -r /usr/local/hadoop/include hadoop@hadoop-slave3:/usr/local/hadoop
+scp -r /usr/local/hadoop/libexec hadoop@hadoop-slave3:/usr/local/hadoop
+scp -r /usr/local/hadoop/logs hadoop@hadoop-slave3:/usr/local/hadoop
+scp -r /usr/local/hadoop/share hadoop@hadoop-slave3:/usr/local/hadoop
+scp -r /usr/local/hadoop/etc hadoop@hadoop-slave3:/usr/local/hadoop
+scp -r /usr/local/hadoop/lib hadoop@hadoop-slave3:/usr/local/hadoop
+scp -r /usr/local/hadoop/slib hadoop@hadoop-slave3:/usr/local/hadoop
+scp -r /usr/local/hadoop/spark hadoop@hadoop-slave3:/usr/local/hadoop
+scp -r /usr/local/hadoop/tmp hadoop@hadoop-slave3:/usr/local/hadoop
 
 hdfs namenode -format
 start-dfs.sh
