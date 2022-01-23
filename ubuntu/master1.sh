6e3e4a3ec11036ffde99d3d474dcc05b227b419e
Arthur Epprecht <arthur.epp@gmail.com>
2022-01-23 17:56:26 -0300

    Update master1.sh

--- ubuntu/master1.sh
+++ ubuntu/master1.sh
@@ -28,12 +28,12 @@ echo 'export PATH=$PATH:$SPARK_HOME/bin' >> ~/.bashrc
 echo 'export YARN_CONF_DIR="/usr/local/hadoop/etc/hadoop"' >> ~/.bashrc
 
 sudo mkdir /install
-sudo wget -O /install/hadoop-3.2.1.tar.gz  "http://ftp.unicamp.br/pub/apache/hadoop/common/hadoop-3.2.1/hadoop-3.2.1.tar.gz"
-sudo wget -O /install/apache-hive-3.1.2-bin.tar.gz  "http://ftp.unicamp.br/pub/apache/hive/hive-3.1.2/apache-hive-3.1.2-bin.tar.gz"
-sudo wget -O /install/sqoop-1.4.7.tar.gz  "http://ftp.unicamp.br/pub/apache/sqoop/1.4.7/sqoop-1.4.7.bin__hadoop-2.6.0.tar.gz"
-sudo wget -O /install/spark-2.4.7-bin-hadoop2.7.tgz  "http://ftp.unicamp.br/pub/apache/spark/spark-2.4.7/spark-2.4.7-bin-hadoop2.7.tgz"
+sudo wget -O /install/hadoop-3.3.1.tar.gz  "http://ftp.unicamp.br/pub/apache/hadoop/common/hadoop-3.3.1/hadoop-3.3.1.tar.gz"
+sudo wget -O /install/apache-hive-3.1.2-bin.tar.gz  "https://dlcdn.apache.org/hive/hive-3.1.2/apache-hive-3.1.2-bin.tar.gz"
+sudo wget -O /install/sqoop-1.4.7.tar.gz  "http://archive.apache.org/dist/sqoop/1.4.7/sqoop-1.4.7.bin__hadoop-2.6.0.tar.gz"
+sudo wget -O /install/spark-3.2.0-bin-hadoop2.7.tgz  "http://ftp.unicamp.br/pub/apache/spark/spark-3.2.0/spark-3.2.0-bin-hadoop3.2.tgz"
 sudo wget -O /install/mysql-connector-java-5.1.47.tar.gz  "https://dev.mysql.com/get/Downloads/Connector-J/mysql-connector-java-5.1.47.tar.gz"
-sudo wget -O /install/zeppelin-0.8.2-bin-all.tgz  "http://ftp.unicamp.br/pub/apache/zeppelin/zeppelin-0.8.2/zeppelin-0.8.2-bin-all.tgz"
+sudo wget -O /install/zeppelin-0.10.0-bin-all.tgz  "http://ftp.unicamp.br/pub/apache/zeppelin/zeppelin-0.10.0/zeppelin-0.10.0-bin-all.tgz"
 
 sudo mkdir -p /usr/local/hadoop
 sudo mkdir -p /usr/local/hadoop/logs
@@ -48,12 +48,12 @@ sudo mkdir -p /usr/local/hadoop/zeppelin
 
 sudo chown -R hadoop:hadoop /usr/local/hadoop
 
-sudo tar -xzvf /install/hadoop-3.2.1.tar.gz -C /usr/local/hadoop --strip-components=1
+sudo tar -xzvf /install/hadoop-3.3.1.tar.gz -C /usr/local/hadoop --strip-components=1
 sudo tar -xzvf /install/apache-hive-3.1.2-bin.tar.gz -C /usr/local/hadoop/hive --strip-components=1
 sudo tar -xzvf /install/sqoop-1.4.7.tar.gz -C /usr/local/hadoop/sqoop --strip-components=1
-sudo tar -xzvf /install/spark-2.4.7-bin-hadoop2.7.tgz -C /usr/local/hadoop/spark --strip-components=1
+sudo tar -xzvf /install/spark-3.2.0-bin-hadoop2.7.tgz -C /usr/local/hadoop/spark --strip-components=1
 sudo tar -xzvf /install/mysql-connector-java-5.1.47.tar.gz -C /install/
-sudo tar -xzvf /install/zeppelin-0.8.2-bin-all.tgz -C /usr/local/hadoop/zeppelin --strip-components=1
+sudo tar -xzvf /install/zeppelin-0.10.0-bin-all.tgz -C /usr/local/hadoop/zeppelin --strip-components=1
 
 sudo cp templates/hive-site.xml /usr/local/hadoop/hive/conf/hive-site.xml
 sudo cp templates/core-site.xml /usr/local/hadoop/etc/hadoop/core-site.xml
